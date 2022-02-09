9e3a0d3e89557a50efaf22d80c516e5a5cf4a4d2
Majulia <majulialcarvalho@gmail.com>
2022-02-09 13:34:31 -0300

    fundamentals

---
+++ Part1.01/Program.cs
@@ -0,0 +1,14 @@
+﻿using static System.Console;
+
+namespace Part1._01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { 
+            WriteLine("Fundamentals");
+
+            
+        }
+    }
+}
