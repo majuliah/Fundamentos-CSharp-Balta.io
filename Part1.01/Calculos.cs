c7561516286e438cd83dade775068e1d1eadc814
Majulia <majulialcarvalho@gmail.com>
2022-02-10 18:07:27 -0300

    relembrando

---
+++ Part1.01/Calculos.cs
@@ -0,0 +1,18 @@
+﻿namespace Part1._01
+{
+    public class Calculos
+    {
+        public int Numero1, Numero2, Numero3;
+
+        public int Calculando(int numero1, int numero2, int numero3)
+        {
+            int resultado = numero1 + numero2 + numero3;
+            return resultado;
+        }
+
+        public override string ToString()
+        {
+            return ($"O resultado é ");
+        }
+    }
+}
--- Part1.01/Program.cs
+++ Part1.01/Program.cs
@@ -8,7 +8,20 @@ namespace Part1._01
         { 
             WriteLine("Fundamentals");
 
+            Calculos calculo = new Calculos();
+            int resultado;
+
+            int num1 = 10;
+            int num2 = 20;
+            int num3 = 30;
             
+            calculo.Calculando(num1, num2, num3);
+
+            resultado = calculo.Calculando(num1, num2, num3);
+            
+            WriteLine($"Resultado: {resultado.ToString()}");
+            
+
         }
     }
 }
