c6935371e5e49fa1df5d3011f375878ace2dc478
Majulia <majulialcarvalho@gmail.com>
2022-03-08 15:34:54 -0300

    Iniciando String

--- Part1.01/Program.cs
+++ Part1.01/Program.cs
@@ -15,7 +15,35 @@ namespace Part1._01
 
             int _int = 20;
             double _double = 30.30;
+
+            byte myByte = 127;
+            sbyte mySbyte = -127;
+            //it understands that the number isnt a number, but a byte
+            // s = signed that means that it receives a sign (-127, -6, -12, etc)
             
+            ushort number = 12;
+            uint number2 = 123; // u means unsigned, the primitive type doesnt accepts numbers with signs (-12, -90)
+            ulong number3 = 1234;
+
+            // short and ushort = 16bits
+            //int and uint = 32bits
+            //long and ulong = 64bits
+
+            float number4 = 12.3f;
+            double number5 = 13.4;
+            decimal number6 = 14.5m;
+            
+            //float 32bits 
+            //double  64bits
+            //decimal  128bits
+
+            bool isTheDayHot = true;
+            //bool 8bit
+
+            char firstLetterOfMyName = 'M';
+            
+
+
         }
     }
 }
