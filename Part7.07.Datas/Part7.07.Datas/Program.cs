b2e5f1d5d32a96017b15cd3b279183f6b77138f7
Majulia <majulialcarvalho@gmail.com>
2022-03-21 16:54:12 -0300

    date types

--- Part7.07.Datas/Part7.07.Datas/Program.cs
+++ Part7.07.Datas/Part7.07.Datas/Program.cs
@@ -8,7 +8,13 @@ namespace Part7._07.Datas
         {
             Console.Clear();
             var data = new DateTime();
+            var data1 = DateTime.Now;
+            var data2 = new DateTime(2020, 10, 12);
+            var data3 = new DateTime(2020, 10, 12);
             Console.WriteLine(data);
+            Console.WriteLine(data1);
+            Console.WriteLine(data3.Year);
+            
         }
     }
 }
