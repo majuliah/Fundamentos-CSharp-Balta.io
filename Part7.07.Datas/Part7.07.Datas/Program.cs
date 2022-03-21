e2a572c45771c512e2fdb02586fc7a995c8ca1fb
Majulia <majulialcarvalho@gmail.com>
2022-03-21 16:46:15 -0300

    beginning date

---
+++ Part7.07.Datas/Part7.07.Datas/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Part7._07.Datas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+            var data = new DateTime();
+            Console.WriteLine(data);
+        }
+    }
+}
