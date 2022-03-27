f865d7ffa5a307b977bf39f9525f45afc3cf90c5
Majulia <majulialcarvalho@gmail.com>
2022-03-27 16:06:12 -0300

    starting exceptions

---
+++ Part10.010.Exceptions/Program.cs
@@ -0,0 +1,12 @@
+﻿using static System.Console;
+
+namespace Part10._010.Exceptions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WriteLine("Hello World!");
+        }
+    }
+}
